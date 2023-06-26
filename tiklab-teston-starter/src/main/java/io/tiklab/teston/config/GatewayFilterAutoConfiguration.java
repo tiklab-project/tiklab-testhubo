b92dcbdb152b90b0ada781a859dbf40cf349572d
孙现诚 <sxc442533106_cheng@qq.com>
2023-06-26 11:13:50 +0800

    配置文件调整

--- tiklab-teston-starter/src/main/java/io/tiklab/teston/config/GatewayFilterAutoConfiguration.java
+++ tiklab-teston-starter/src/main/java/io/tiklab/teston/config/GatewayFilterAutoConfiguration.java
@@ -30,32 +30,6 @@ public class GatewayFilterAutoConfiguration {
     //路由配置
     @Bean
     RouterConfig routerConfig(){
-//        String[] s = {
-//                "/user",
-//                "/eam",
-//                "/appLink",
-//                "/todo/deletetodo",
-//                "/todo/updatetodo",
-//                "/todo/detailtodo",
-//                "/todo/findtodopage",
-//
-//                "/message/message",
-//                "/message/messageItem",
-//                "/message/messageReceiver",
-//
-//                "/oplog/deletelog",
-//                "/oplog/updatelog",
-//                "/oplog/detaillog",
-//                "/oplog/findlogpage",
-//        };
-//
-//        if (enableEam){
-//            s = new String[]{};
-//        }
-//
-//        return RouterConfigBuilder.instance()
-//                .preRoute(s, authAddress)
-//                .get();
         return RouterConfigBuilder.instance()
                 .preRoute(new String[]{}, authAddress)
                 .get();
