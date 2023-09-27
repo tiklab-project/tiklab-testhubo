85cf40733184513906b26b8de8b3d72a3cf528c7
孙现诚 <sxc442533106_cheng@qq.com>
2023-09-27 19:04:06 +0800

    发版

--- tiklab-teston-starter/src/main/java/io/tiklab/teston/TestOnApplication.java
+++ tiklab-teston-starter/src/main/java/io/tiklab/teston/TestOnApplication.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @EnableTestOn
+@EnableScheduling
 @PropertySource(value = "classpath:application.yaml",factory = PropertyAndYamlSourceFactory.class)
 public class TestOnApplication {
 
