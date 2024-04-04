9faf220206283138e7af01438b15a284e303dfcf
孙现诚 <sxc442533106_cheng@qq.com>
2024-04-04 19:33:21 +0800

    static

--- thoughtware-teston-api/src/main/java/io/thoughtware/teston/test/test/model/TestCase.java
+++ thoughtware-teston-api/src/main/java/io/thoughtware/teston/test/test/model/TestCase.java
@@ -64,11 +64,11 @@ public class TestCase extends BaseModel{
     private User updateUser;
 
     @ApiProperty(name="createTime",desc="创建时间")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private java.sql.Timestamp createTime;
 
     @ApiProperty(name="updateTime",desc="更新时间")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private java.sql.Timestamp updateTime;
 
     @ApiProperty(name="sort",desc="排序")
