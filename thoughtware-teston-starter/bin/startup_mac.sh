6bf81b126ed82ac41a0b5d9637bba911ed31474e
孙现诚 <sxc442533106_cheng@qq.com>
2024-04-30 09:45:13 +0800

    static

--- thoughtware-teston-starter/bin/startup_mac.sh
+++ thoughtware-teston-starter/bin/startup_mac.sh
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------------------------------------
 DIRS=$(dirname "$PWD")
 
-APP_MAIN="io.thoughtware.eas.starter.EasApplication"
+APP_MAIN="io.thoughtware.teston.TestOnApplication"
 
 JDK_VERSION=jdk-16.0.2
 PGSQL_VERSION=pgsql-10.23
--- thoughtware-teston-starter/bin/status_mac.sh
+++ thoughtware-teston-starter/bin/status_mac.sh
@@ -8,7 +8,7 @@ if [ -e "${DIRS}/${JDK_VERSION}" ]; then
       JAVA_HOME="${DIRS}/${JDK_VERSION}"
 fi
 
-APP_MAIN="io.thoughtware.eas.starter.EasApplication"
+APP_MAIN="io.thoughtware.teston.TestOnApplication"
 
 PID=0
 getPID(){
