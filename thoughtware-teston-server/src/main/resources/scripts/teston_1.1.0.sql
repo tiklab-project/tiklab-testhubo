e7b86425c1a6636a5b413b9ebe45b340479c7ccc
孙现诚 <sxc442533106_cheng@qq.com>
2024-06-16 17:32:26 +0800

    yaml agent 配置

--- thoughtware-teston-server/src/main/resources/scripts/teston_1.1.0.sql
+++ thoughtware-teston-server/src/main/resources/scripts/teston_1.1.0.sql
@@ -12,3 +12,15 @@ ALTER TABLE teston_agent_config DROP COLUMN number;
 
 -- 重命名create_time列为update_time
 ALTER TABLE teston_agent_config RENAME COLUMN create_time TO update_time;
+
+-- 添加新列 是否启用 0不启用，1启用
+ALTER TABLE teston_agent_config ADD COLUMN enable int;
+-- 将新列的所有值设置为 1
+UPDATE teston_agent_config SET enable = 1;
+
+
+-- 添加新列type： function功能，auto自动化
+ALTER TABLE teston_test_plan ADD COLUMN type VARCHAR(32);
+
+-- 将新列的所有值设置为 'function'
+UPDATE teston_test_plan SET type = 'function';
