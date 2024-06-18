20c7b59b5a0dd94d44d32a16d929c8329ee8544a
孙现诚 <sxc442533106_cheng@qq.com>
2024-06-18 20:28:15 +0800

    static

--- thoughtware-teston-server/src/main/java/io/thoughtware/teston/test/common/wsTest/controller/WebSocketRegisterApi.java
+++ thoughtware-teston-server/src/main/java/io/thoughtware/teston/test/common/wsTest/controller/WebSocketRegisterApi.java
@@ -1,14 +1,25 @@
 package io.thoughtware.teston.test.common.wsTest.controller;
 
 import io.thoughtware.teston.test.common.wsTest.service.WebSocketServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketRegisterApi implements WebSocketConfigurer {
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(1024 * 128); // 设置文本消息缓冲区大小
+        container.setMaxBinaryMessageBufferSize(1024 * 128); // 设置二进制消息缓冲区大小
+        return container;
+    }
+
     /**
      * 注册handle
      */
@@ -18,4 +29,7 @@ public class WebSocketRegisterApi implements WebSocketConfigurer {
                 .addInterceptors()
                 .setAllowedOrigins("*");
     }
+
+
+
 }
