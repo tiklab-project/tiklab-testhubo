a71ef244093194174ae86043e9aff7ea54c1210b
孙现诚 <sxc442533106_cheng@qq.com>
2024-08-12 17:46:38 +0800

    static

--- thoughtware-teston-server/src/main/java/io/thoughtware/teston/test/common/wsTest/service/WebSocketServiceImpl.java
+++ thoughtware-teston-server/src/main/java/io/thoughtware/teston/test/common/wsTest/service/WebSocketServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.*;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.*;
 
